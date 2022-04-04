606c2003314fa92f5feb228330f1af9a7bca7e2b
Firend Endlire <demianvlas@gmail.com>
2022-04-04 21:31:41 +0300

    Добавил новые папки и начал основную работу

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+app = Flask(__name__, template_folder="html_templates", static_folder="static_content" )
+
+
+@app.route('/')
+@app.route('/start_page')
+def start_page():
+    return render_template('Start.html')
+
+
+@app.route('/registration_page')
+def registration_page():
+    return render_template('Registration.html')
+
+
+@app.route('/log_in_page')
+def log_in_page():
+    return render_template('Log_in.html')
+
+
+@app.route('/converter_page')
+def converter_page():
+    return render_template('Converter.html')
+
+
+if __name__ == '__main__':
+    app.run()
+    """port=8080, host='127.0.0.1'"""
+    
